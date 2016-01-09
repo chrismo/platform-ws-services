ca7490083dbc61d257f657913e404da9bbe49588
Chris Morris <chris.morris@livingsocial.com>
2016-01-09 16:59:55 -0600

    Added test for many checks from CurrentChecks

--- model_deployment_test.go
+++ model_deployment_test.go
@@ -1,9 +1,6 @@
 package main
 
-import (
-	"log"
-	"testing"
-)
+import "testing"
 
 func TestIdempotentDeployment(t *testing.T) {
 	setupTestDB()
@@ -26,10 +23,25 @@ func TestCurrentChecksWithOneCheck(t *testing.T) {
 	defer tearDownTestDB()
 	c := Check{Type: "type", Name: "name"}
 	equals(t, nil, c.Save())
-	d := Deployment{Id: "123", GroupId: "g123", Type: "type", Name: "name",
+	d := Deployment{Id: "234", GroupId: "g234", Type: "type", Name: "name",
 		Checks: []Check{c},
 	}
-	_, err := d.CurrentChecks()
-	log.Print(err)
+	checks, err := d.CurrentChecks()
 	equals(t, nil, err)
+	equals(t, 1, len(checks))
+}
+
+func TestCurrentChecksWithManyChecks(t *testing.T) {
+	setupTestDB()
+	defer tearDownTestDB()
+	checkA := Check{Type: "type", Name: "nameA"}
+	checkB := Check{Type: "type", Name: "nameB"}
+	equals(t, nil, checkA.Save())
+	equals(t, nil, checkB.Save())
+	d := Deployment{Id: "345", GroupId: "g345", Type: "type", Name: "name",
+		Checks: []Check{checkA, checkB},
+	}
+	checks, err := d.CurrentChecks()
+	equals(t, nil, err)
+	equals(t, 2, len(checks))
 }
